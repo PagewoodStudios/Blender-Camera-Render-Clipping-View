1c3df45dc8bc0b6899352fee9f73e85dc72aba30
AdamEarle <MrAdamEarle@gmail.com>
2018-02-19 21:48:47 +1100

    Updated Credits

--- Blender-Camera-Render-Clipping-View.py
+++ Blender-Camera-Render-Clipping-View.py
@@ -1,11 +1,19 @@
 '''
-Overviiew:
+Overview:
 
     1.Clipping boarder hotkey "alt+B" to do the same thing as a camera in the 3D viewport
     2.The Clipping boarder needs to travel along with the camera
     3.The ability to update on everyframe for rendering to help reduce memory
         This should be kept as a secondary option for rendering and working with large scene data
         The current system is really great for sculpting and the can be built on top of what is already existing
+
+
+
+
+Credits:
+    Pagewood Studios - www.PagewoodStudios.com
+    Adam Earle
+
 
 
 
